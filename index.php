4f625a41f0161f3a96a5704cbab2b4d61221b982
Enrique Perez Clavier <eaperezc@gmail.com>
2014-03-05 12:46:32 -0430

    como php

---
+++ index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<link rel="stylesheet" type="text/css" href="shadowbox-3.0.3/shadowbox.css">
+<script type="text/javascript" src="shadowbox-3.0.3/shadowbox.js"></script>
+<script type="text/javascript">
+Shadowbox.init();
+</script>
+<title>Santiago´s Portfolio</title>
+</head>
+
+<body>
+
+<div id="head" ><img id="titleitem1" src="title.png" /> 
+<ul>
+    	<li><a href="index.html">3D</a></li>
+        <li id="ilSlash">/</li>
+        <li><a href="http://www.youtube.com/v/mCegb4LODd4" rel="shadowbox;height=540;width=960"</a>Modeling Reel</li>
+        <li id="ilSlash">/</li>
+        <li><a href="web.html">Web Design</a></li>
+        <li id="ilSlash">/</li>
+        <li><a href="about.html">About Me</a></li>
+    </ul>
+</div>
+
+
+<div>  </div>
+
+<div id="container" class="js-masonry" 
+  data-masonry-options='{ "columnWidth": 0, "itemSelector": ".item" }'>
+  
+  <a href="images/01.jpg" rel="shadowbox" title="Asian Warrior"><img class="item" src="images/01.jpg" /></a>
+  
+  <a href="images/ORC3s_o.jpg" rel="shadowbox" title="Orc Warcry"><img class="item" src="images/ORC3s_o.jpg" /></a>
+  
+  <a href="images/4.jpg" rel="shadowbox" title="Orc Bust"><img class="item w2" src="images/4.jpg" /></a>
+  
+  <a href="images/Birdion01.jpg" rel="shadowbox" title="Birdion Front"><img class="item" src="images/Birdion01.jpg" /></a>
+  
+  <a href="images/Turn.jpg" rel="shadowbox" title="Cyborg (WIP)"><img class="item w2" src="images/Turn.jpg" /></a>
+  
+  <a href="images/WomanFace01.jpg" rel="shadowbox" title="Woman Face"><img class="item" src="images/WomanFace01.jpg" /></a>
+  
+  <a href="images/WomanFace02.jpg" rel="shadowbox" title="Woman Hair"><img class="item" src="images/WomanFace02.jpg" /></a>
+  
+  <a href="images/BARBARIAN01.jpg" rel="shadowbox" title="Cornered Barbarian"><img class="item w2" src="images/BARBARIAN01.jpg" /></a>
+  
+  <a href="images/Quagflier00.jpg" rel="shadowbox[quagflier]" title="Quagflier"><img class="item w2" src="images/Quagflier00.jpg" /></a>
+  
+  <a href="images/Warrior3_o.jpg" rel="shadowbox" title="Warrior Armor"><img class="item w2" src="images/Warrior3_o.jpg" /></a>
+  
+  <a href="images/Render01_o.jpg" rel="shadowbox" title="En la Barra Mall Kiosk"><img class="item w2" src="images/Render01_o.jpg" /></a>
+  
+  <a href="images/Warrior2_o.jpg" rel="shadowbox" title="Warrior Pose"><img class="item w2" src="images/Warrior2_o.jpg" /></a>
+  
+  <a href="images/Paladin.jpg" rel="shadowbox" title="Paladin, Diablo III Contest"><img class="item w2" src="images/Paladin.jpg" /></a>
+  
+  <a href="images/Birdion02.jpg" rel="shadowbox" title="Birdion 3/4"><img class="item w2" src="images/Birdion02.jpg" /></a>
+  
+  <a href="images/GORGAMEK6_o.jpg" rel="shadowbox" title="Gorgamek Color"><img class="item" src="images/GORGAMEK6_o.jpg" /></a>
+  
+  <a href="../../Documents/PORTAFOLIO/3D/GORGAMEK5_o.jpg" rel="shadowbox" title="Gorgamek Wireframe"><img class="item" src="images/GORGAMEK5_o.jpg" /></a>
+  
+  <a href="images/YUM_o.jpg" rel="shadowbox" title="Yum Kaax, God of Hunt"><img class="item w2" src="images/YUM_o.jpg" /></a>
+  
+  <a href="images/YUM-C_o.jpg" rel="shadowbox" title="Yum Cimil, God of Death"><img class="item" src="images/YUM-C_o.jpg" /></a>
+  
+  <a href="images/KUKU_o.jpg" rel="shadowbox" title="Kukulkhan, Supreme Mayan God"><img class="item" src="images/KUKU_o.jpg" /></a>
+  
+  <a href="images/waterfall5_o.jpg" rel="shadowbox" title="Pocket Pet, Unity 3D Demo"><img class="item w2" src="images/waterfall5_o.jpg" /></a>
+  
+  <a href="images/Overlord.jpg" rel="shadowbox" title="Overlord"><img class="item w2" src="images/Overlord.jpg" /></a>
+  
+  <a href="../../Documents/PORTAFOLIO/3D/TruckFinal2_o.jpg" rel="shadowbox" title="Truck Modeling Tutorial"><img class="item w2" src="images/TruckFinal2_o.jpg" /></a>
+  
+  <a href="images/Mech01_2L.jpg" rel="shadowbox" title="2 Legged Mech, Unity Game (WIP)"><img class="item" src="images/Mech01_2L.jpg" /></a>
+  
+  <a href="images/Mech01_4L.jpg" rel="shadowbox" title="4 Legged Mech, Unity Game (WIP)"><img class="item" src="images/Mech01_4L.jpg" /></a>
+  
+  <a href="images/IMAGE01.jpg" rel="shadowbox[protester]" title="Captain Homeland"><img class="item w2" src="images/IMAGE01.jpg" /></a>
+  
+  <a href="images/Angel01.jpg" rel="shadowbox" title="Angel"><img class="item" src="images/Angel01.jpg" /></a>
+  
+  <a href="images/face.jpg" rel="shadowbox" title="Angel Face"><img class="item" src="images/face.jpg" /></a>
+  
+  <a href="images/simon bolivar_o.jpg" rel="shadowbox" title="Simon Bolivar"><img class="item w2" src="images/simon bolivar_o.jpg" /></a>
+  
+  <a href="images/KIS_o.jpg" rel="shadowbox" title="Kisin, God of War"><img class="item w2" src="images/KIS_o.jpg" /></a>
+  
+  <a href="images/Batman.jpeg" rel="shadowbox" title="Toony Batman (armored)"><img class="item w2" src="images/Batman.jpeg" /></a>
+  
+  <a href="images/SOLDIER1_o.jpeg" rel="shadowbox" title="Soldier"><img class="item w2" src="images/SOLDIER1_o.jpeg" /></a>
+  
+<a style="display:none" href="images/Quagflier.jpg" rel="shadowbox[quagflier]" title="Soldier"><img class="item w2" src="images/Quagflier.jpg" /></a>
+
+<a style="display:none" href="images/IMAGE02.jpg" rel="shadowbox[protester]" title="Soldier"><img class="item w2" src="images/IMAGE02.jpg" /></a>
+</div>
+
+
+<div id="footer"> Last Update, February 28,2014</div>
+
+
+<script type="text/javascript" src="masonry.pkgd.min.js"></script>
+
+
+</body>
+</html>
